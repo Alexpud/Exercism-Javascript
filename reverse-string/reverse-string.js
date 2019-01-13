7b2b622f17d2a5980503060f81723a3b1e9122a8
Alexandre <alexandre.maciele@gmail.com>
2019-01-13 16:09:47 -0200

    Improvement found in other fantastic solutions

--- reverse-string/reverse-string.js
+++ reverse-string/reverse-string.js
@@ -1,11 +1,6 @@
 var reverseString = function(string)
 {
-    var reversedString = "";
-    for (var i = string.length - 1; i >= 0; i--)
-    {
-        reversedString += string[i];
-    }
-    return reversedString;
+    return string.split("").reverse().join("");
 }
 
 export {reverseString};
