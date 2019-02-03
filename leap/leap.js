74587b6ee2ffe21f08ddda1df59564afee257441
Alexandre <alexandre.maciele@gmail.com>
2019-02-03 16:09:50 -0200

    Fix because I was lazy

--- leap/leap.js
+++ leap/leap.js
@@ -2,9 +2,5 @@ export const isLeap = (year) => {
     var isDivisibleBy4 = year % 4 == 0;
     var isEvenlyDivisibleBy100 = year % 100 == 0;
     var isEvenlyDivisibleBy400 = year % 400 == 0;
-    if (isDivisibleBy4)
-    {
-        return !isEvenlyDivisibleBy100 || isEvenlyDivisibleBy400;
-    }
-    return false;
+    return isDivisibleBy4 && (!isEvenlyDivisibleBy100 || isEvenlyDivisibleBy400)
 };
