bd8a680df93553317b4b73572ec91e120bc089bb
Alexandre <alexandre.maciele@gmail.com>
2019-04-09 21:41:15 -0300

    Refactoring of resistor-color solution

--- resistor-color/resistor-color.js
+++ resistor-color/resistor-color.js
@@ -1,8 +1,6 @@
 const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
-var collorMapping = [];	
-COLORS.map((color, index) => { collorMapping[color] = index; })
 var colorCode = function(color) {
-	return collorMapping[color];
+	return COLORS.indexOf(color);
 };
 
 module.exports = { colorCode, COLORS };
