1f0d22c82b6de1dc1524709108c58b304394a606
alexandremaciel <alexandre.maciel@gmail.com>
2019-09-02 16:24:03 -0300

    Final alterations, hopefuly

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -4,16 +4,16 @@
 //
 
 let Colors = {
-  Black: 0,
-  Brown: 1,
-  Red: 2,
-  Orange: 3,
-  Yellow: 4,
-  Green: 5,
-  Blue: 6,
-  Violet: 7,
-  Grey: 8,
-  White: 9
+  black: 0,
+  brown: 1,
+  red: 2,
+  orange: 3,
+  yellow: 4,
+  green: 5,
+  blue: 6,
+  violet: 7,
+  grey: 8,
+  white: 9
 };
 
 export const value = (colors) => {
@@ -22,12 +22,8 @@ export const value = (colors) => {
   return Number(colorsNumberRepresentation);
 };
 
-const capitalize = (s) => {
-  return s.charAt(0).toUpperCase() + s.slice(1)
-}
-
 const colorNumberRepresentationLookUp = (color) => {
-  let colorValue = Colors[capitalize(color)];
+  let colorValue = Colors[color];
   return colorValue;
 }
 
